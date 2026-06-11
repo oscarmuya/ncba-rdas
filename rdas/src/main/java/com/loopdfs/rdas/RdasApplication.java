9a119550c7f57e040037e3fd10bf3389bc6a4d6b
Oscar Muya <oscarmuyalx@gmail.com>
2026-06-11 15:20:02 +0300

    Initial Commit

---
+++ rdas/src/main/java/com/loopdfs/rdas/RdasApplication.java
@@ -0,0 +1,13 @@
+package com.loopdfs.rdas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdasApplication.class, args);
+	}
+
+}
